0f12bf19bd1da50398589f294aa58062fa7b31b5
Marcin <Majsyn@gmail.com>
2018-07-02 18:59:30 +0200

    dodanie nowych lini tekstu

--- src/HelloWorldJavaStart.java
+++ src/HelloWorldJavaStart.java
@@ -2,6 +2,10 @@ public class HelloWorldJavaStart {
 
     public static void main(String[] args) {
         System.out.println("GIT");
+        System.out.println("GIT");
+        System.out.println("GIT");
+        System.out.println("GIT");
+        System.out.println("GIT");
 
     }
 }
